809c75f673f62b9e093f58fbf62180b4e8360353
Dzesika Tkaczyk <dzesika.tkaczyk@gmail.com>
2019-08-20 22:41:05 +0200

    add flags and capitals

---
+++ js/script.js
@@ -0,0 +1,33 @@
+var url = 'https://restcountries.eu/rest/v2/name/';
+var countriesList = document.getElementById('countries');
+
+document.getElementById('search').addEventListener('click', searchCountries);
+
+function searchCountries() {
+    var countryName = document.getElementById('country-name').value;
+    if(!countryName.length) countryName = 'Poland';
+    fetch(url + countryName)
+        .then(function(resp) {
+            return resp.json();
+        })
+        .then(showCountriesList);
+}
+
+function showCountriesList(resp) {
+ 	countriesList.innerHTML = '';
+ 	resp.forEach(function(item) {
+  		var country = document.createElement('h1');
+    	country.innerText = item.name;
+    	countriesList.appendChild(country);
+    	var capital = document.createElement('li');
+    	capital.innerText = "Capital city: " + item.capital;
+    	countriesList.appendChild(capital);
+    	var population = document.createElement('li');
+    	population.innerText = "Population: " + item.population;
+    	countriesList.appendChild(population);
+    	var flag = document.createElement('img');
+    	flag.src = item.flag;
+    	countriesList.appendChild(flag);
+	});
+}
+
