aefd3c4bf84605eabc6bb55263fabd819ecbcb8a
Dzesika Tkaczyk <dzesika.tkaczyk@gmail.com>
2019-08-22 19:23:05 +0200

    server not responding

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ document.getElementById('search').addEventListener('click', searchCountries);
 
 function searchCountries() {
     var countryName = document.getElementById('country-name').value;
-    if(!countryName.length) countryName = 'Poland';
+    if (countryName.length <= 0) { countryName = 'Poland'; }
     fetch(url + countryName)
         .then(function(resp) {
             return resp.json();
@@ -14,20 +14,25 @@ function searchCountries() {
 }
 
 function showCountriesList(resp) {
- 	countriesList.innerHTML = '';
- 	resp.forEach(function(item) {
-  		var country = document.createElement('h1');
-    	country.innerText = item.name;
-    	countriesList.appendChild(country);
-    	var capital = document.createElement('li');
-    	capital.innerText = "Capital city: " + item.capital;
-    	countriesList.appendChild(capital);
-    	var population = document.createElement('li');
-    	population.innerText = "Population: " + item.population;
-    	countriesList.appendChild(population);
-    	var flag = document.createElement('img');
-    	flag.src = item.flag;
-    	countriesList.appendChild(flag);
-	});
+    if (Array.isArray(resp)){
+        countriesList.innerHTML = '';
+        resp.forEach(function(item) {
+        	var country = document.createElement('h1');
+        	country.innerText = item.name;
+        	countriesList.appendChild(country);
+        	var capital = document.createElement('li');
+        	capital.innerText = "Capital city: " + item.capital;
+        	countriesList.appendChild(capital);
+        	var population = document.createElement('li');
+        	population.innerText = "Population: " + item.population;
+        	countriesList.appendChild(population);
+        	var flag = document.createElement('img');
+        	flag.src = item.flag;
+        	countriesList.appendChild(flag);
+    	});
+    } else {
+        countriesList.innerHTML = 'server side error or country not found';
+    }
 }
+
 
